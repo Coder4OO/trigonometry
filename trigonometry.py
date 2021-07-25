4f3e0ad32e2a5cbc5590acd139b88dcfc99838bf
Coder400 <67166289+Coder4OO@users.noreply.github.com>
2021-07-25 13:43:20 +0100

    Add files via upload
    fixed math.deg to math.degrees

--- trigonometry.py
+++ trigonometry.py
@@ -1,28 +1,30 @@
 import math
 
 def get_sine_angle(o, h):
-	return math.deg(math.asin(o/h))
+	return math.degrees(math.asin(o/h))
 	
 def get_cosine_angle(a, h):
-	return math.deg(math.acos(a/h))
+	return math.degrees(math.acos(a/h))
 
 def get_tangent_angle(o, a):
-	return math.deg(math.atan(o/a))
+	return math.degrees(math.atan(o/a))
 	
 def get_opposite_from_sine(h, ang):
-	return math.deg(math.sin(ang))*h
+	return math.degrees(math.sin(ang))*h
 	
 def get_hypotenuse_from_sine(o, ang):
-	return o/math.deg(math.sin(ang))
+	return o/math.degrees(math.sin(ang))
 	
 def get_adjacent_from_cosine(h, ang):
-	return math.deg(math.cos(ang))*h
+	return math.degrees(math.cos(ang))*h
 	
 def get_hypotenuse_from_cosine(a, ang):
-	return a/math.deg(math.cos(ang))
+	return a/math.degrees(math.cos(ang))
 	
 def get_opposite_from_tangent(a, ang):
-	return math.deg(math.tan(ang))*a
+	return math.degrees(math.tan(ang))*a
 	
 def get_adjacent_from_tangent(o, ang):
-	return o/math.deg(math.tan(ang))
+	return o/math.degrees(math.tan(ang))
+	
+print(get_sine_angle(4,5))
